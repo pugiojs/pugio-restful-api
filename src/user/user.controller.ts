fb438baa7e5798ba52910f164eda0061fa5bfa85
lenconda <i@lenconda.top>
2021-11-29 15:01:58 +0800

    fix: 🐛 remove unused code

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,5 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { Permissions } from 'src/permissions/permissions.decorator';
-import { PermissionsGuard } from 'src/permissions/permissions.guard';
 import { CurrentUser } from './user.decorator';
 
 @Controller('/user')
@@ -10,12 +8,5 @@ export class UserController {
     @Get('/profile')
     getUserProfile(@CurrentUser() user) {
         return user;
-    }
-
-    @UseGuards(AuthGuard(), PermissionsGuard)
-    @Get('/test')
-    @Permissions('test:1')
-    async getTest() {
-        return {};
     }
 }
