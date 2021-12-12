78c7d19a454dc3d23153ca5cc2fa2bb48c9739f2
lenconda <i@lenconda.top>
2021-12-12 17:57:48 +0800

    fix: 🐛 create key pair

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,43 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import * as fs from 'fs-extra';
-import { generateKeyPairSync } from 'crypto';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class AppService {
-    public constructor(
-        private readonly configService: ConfigService,
-    ) {}
-
-    public createKeyPair() {
-        const {
-            keyPairPathname,
-            publicKeyPathname,
-            privateKeyPathname,
-        } = this.configService.get('sign');
-
-        const {
-            publicKey,
-            privateKey,
-        } = generateKeyPairSync('rsa', {
-            modulusLength: 4096,
-            publicKeyEncoding: {
-                type: 'spki',
-                format: 'pem',
-            },
-            privateKeyEncoding: {
-                type: 'pkcs8',
-                format: 'pem',
-            },
-        });
-
-        if (!fs.existsSync(keyPairPathname)) {
-            fs.mkdirpSync(keyPairPathname);
-        }
-
-        if (!fs.existsSync(publicKeyPathname) || !fs.existsSync(privateKeyPathname)) {
-            fs.writeFileSync(publicKeyPathname, publicKey);
-            fs.writeFileSync(privateKeyPathname, privateKey);
-        }
-    }
-}
+export class AppService {}
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,46 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
-
 import { AppModule } from './app.module';
-import { AppService } from './app.service';
+import getSignConfig from './config/sign.config';
+import { generateKeyPairSync } from 'crypto';
+import * as fs from 'fs-extra';
+
+const createKeyPair = (signConfig: ReturnType<typeof getSignConfig>) => {
+    const {
+        keyPairPathname,
+        publicKeyPathname,
+        privateKeyPathname,
+    } = signConfig;
+
+    const {
+        publicKey,
+        privateKey,
+    } = generateKeyPairSync('rsa', {
+        modulusLength: 4096,
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'pem',
+        },
+        privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'pem',
+        },
+    });
+
+    if (!fs.existsSync(keyPairPathname)) {
+        fs.mkdirpSync(keyPairPathname);
+    }
+
+    if (!fs.existsSync(publicKeyPathname) || !fs.existsSync(privateKeyPathname)) {
+        fs.writeFileSync(publicKeyPathname, publicKey);
+        fs.writeFileSync(privateKeyPathname, privateKey);
+    }
+};
 
 async function bootstrap() {
+    createKeyPair(getSignConfig());
     const app = await NestFactory.create(AppModule);
     const configService = app.get<ConfigService>(ConfigService);
-    const appService = app.get<AppService>(AppService);
-    await appService.createKeyPair();
     app.setGlobalPrefix('/api/v1');
     await app.listen(
         configService.get<number>('app.port'),
