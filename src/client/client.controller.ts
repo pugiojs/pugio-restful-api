e28abca25f5feade8e92444958591d41c3a4ab1f
lenconda <i@lenconda.top>
2022-01-09 16:54:36 +0800

    feat: 🎸 optimize code

--- src/client/client.controller.ts
+++ src/client/client.controller.ts
@@ -1,4 +1,9 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+    Controller,
+    Get,
+    Param,
+    Query,
+} from '@nestjs/common';
 import { ClientService } from './client.service';
 
 @Controller('/client')
