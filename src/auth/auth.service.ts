c64fc3a4d15abea61d46ec7cc487d27c9fef64eb
lenconda <i@lenconda.top>
2022-05-16 15:58:10 +0800

    feat: 🎸 file downloader

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -30,7 +30,7 @@ export class AuthService {
         return new Promise((resolve, reject) => {
             switch (type.toLocaleLowerCase()) {
                 case 'ak': {
-                    this.keyService.validateApiKey(token, ['socket'], 'all')
+                    this.keyService.validateApiKey(token)
                         .then((user) => {
                             if (_.isString(user?.id)) {
                                 resolve(user.id);
