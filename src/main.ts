bda2f3a47e184a75da7d55a1ea239ad6e2481c38
lenconda <i@lenconda.top>
2022-05-27 11:11:44 +0800

    fix: 🐛 entity too large issue

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+
     app.enableCors({
         allowedHeaders: '*',
         origin: '*',
     });
+    app.use(bodyParser.json({ limit: '50mb' }));
+    app.use(bodyParser.urlencoded( {limit: '50mb', extended: true }));
 
     const configService = app.get<ConfigService>(ConfigService);
     app.setGlobalPrefix('/api/v1');
